e946757821ee9d990c050971504a850750bd6e8e
BiancaLouiseDantic <133474343+BiancaLouiseDantic@users.noreply.github.com>
2023-11-30 23:11:36 +0800

    loop through each option image element

---
+++ Rock-Paper-Scissors-Game/script.js
@@ -0,0 +1,22 @@
+//Get to DOM elements
+const gameContainer = document.querySelector(".container"),
+userResult = document.querySelector(".user_result img"),
+cpuResult = document.querySelector(".cpu_result img"),
+result = document.querySelector(".result"),
+optionImages = document.querySelectorAll(".option_image");
+
+//Loop thrpugh each option image element
+optionImages.forEach((image, index) => {
+  image.addEventListener("click", (e) => {
+    image.classList.add("active");
+
+
+    //Loop through each option image again
+    optionImages.forEach((image2, index2) => {
+      //if the current index doesn't match the clicked index
+      //Remove the "active" class from the other option images
+      index !== index2 && image2.classList.remove("active");
+    });
+    console.log(e.target)
+  });
+});
